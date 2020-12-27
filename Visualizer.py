e01b8ad4072d406bb08e47f418c713c556ce3315
EricHuang20 <sphericyt@gmail.com>
2020-12-27 15:50:33 -0500

    Implemented A* Algorithm

--- Visualizer.py
+++ Visualizer.py
@@ -60,7 +60,18 @@ class Node:
         pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
 
     def updateNeighbours(self, grid):
-        pass
+        self.neighbours = []
+        if self.row < self.rows - 1 and not grid[self.row + 1][self.col].isWall(): #down
+            self.neighbours.append(grid[self.row + 1][self.col])
+
+        if self.row > 0 and not grid[self.row - 1][self.col].isWall(): #up
+            self.neighbours.append(grid[self.row - 1][self.col])
+
+        if self.col < self.rows - 1 and not grid[self.row][self.col + 1].isWall(): #right
+            self.neighbours.append(grid[self.row][self.col + 1])
+
+        if self.row > 0 and not grid[self.row][self.col - 1].isWall(): #left
+            self.neighbours.append(grid[self.row][self.col - 1])
 
     def __lt__(self, other):
         return False
@@ -69,6 +80,57 @@ def dist(p1,p2):
     x1, y1 = p1
     x2, y2 = p2
     return abs(x1-x2) + abs(y1-y2)
+
+def reconstructPath(cameFrom, end, draw):
+    while end in cameFrom:
+        end = cameFrom[end]
+        end.makePath()
+        draw()
+
+def algorithm(draw, grid, start, end):
+    count = 0
+    openSet = PriorityQueue()
+    openSet.put((0, count, start))
+    cameFrom = {}
+    gScore = {node: float("inf") for row in grid for node in row}
+    gScore[start] = 0
+
+    fScore = {node: float("inf") for row in grid for node in row}
+    fScore[start] = dist(start.getPos(), end.getPos())
+
+    openSetHash = {start}
+
+    while not openSet.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        
+        current = openSet.get()[2]
+        openSetHash.remove(current)
+        if current == end: #make path
+            reconstructPath(cameFrom, end, draw)
+            end.makeEnd()
+            start.makeStart()
+            return True
+
+        for neighbour in current.neighbours:
+            temp_gScore = gScore[current] + 1
+            if temp_gScore < gScore[neighbour]:
+                cameFrom[neighbour] = current
+                gScore[neighbour] = temp_gScore
+                fScore[neighbour] = temp_gScore + dist(neighbour.getPos(), end.getPos())
+                if neighbour not in openSetHash:
+                    count += 1
+                    openSet.put((fScore[neighbour], count, neighbour))
+                    openSetHash.add(neighbour)
+                    neighbour.makeOpen()
+                
+        draw()
+
+        if current != start:
+            current.makeClosed()
+
+    return False
 
 def makeGrid(rows, width):
     grid = []
@@ -114,31 +176,49 @@ def main(win, width):
     end = None
 
     run = True
-    searching = False
+    
     while run:
         draw(win, grid, rows, width)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            if searching:
-                continue
+
             if pygame.mouse.get_pressed()[0]:
                 pos = pygame.mouse.get_pos()
                 row, col = getClickedPos(pos, rows, width)
                 node = grid[row][col]
-                if not start:
+                if not start and node != end:
                     start = node
                     start.makeStart()
-                elif not end:
+                elif not end and node != start:
                     end = node
                     end.makeEnd()
-                elif (node != start) and (node != end):
+                elif node != start and node != end:
                     node.makeWall()
 
             elif pygame.mouse.get_pressed()[2]:
-                pass
+                pos = pygame.mouse.get_pos()
+                row, col = getClickedPos(pos, rows, width)
+                node = grid[row][col]
+                node.reset()
+                if node == start:
+                    start = None
+                elif node == end:
+                    end = None
 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and start and end:
+                    for row in grid:
+                        for node in row:
+                            node.updateNeighbours(grid)
+                    algorithm(lambda: draw(win, grid, rows, width), grid, start, end)
+
+                if event.key == pygame.K_BACKSPACE:
+                    start = None
+                    end = None
+                    grid = makeGrid(rows, width)
                 
+            
 
     pygame.quit()
 
